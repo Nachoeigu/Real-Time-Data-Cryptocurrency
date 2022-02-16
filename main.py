aee1f3dc678a49c127ebdb069644db7ffaa7e662
Nacho Eigu <ignacio.eiguren@gmail.com>
2022-02-16 16:11:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from model import Coins
 
 #We initialize the process
-real_time_coins = Coins(mode = True) #True = Coingeko, False = Coinmarketcap
+real_time_coins = Coins(mode = False) #True = Coingeko, False = Coinmarketcap
 
 #We extract the data in real time
 real_time_coins.extraction()
